d1440440dc2c61899a7d10a973a35fe316c7615d
ivanyuchen <ivanyuchen@gmail.com>
2025-12-30 00:48:57 +0800

    first commit

---
+++ tetris/assets/tile_map_layer.gd
@@ -0,0 +1,16 @@
+extends TileMapLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("ready")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+var i = []
+
+var block = [i,o,j,l,z,s,t]
